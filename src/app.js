f9d66a18215b26b137e96cf53e67565e5da8e824
Edgar Méndez <mdzredgar@gmail.com>
2025-10-08 19:58:58 -0500

    added: models for users. register in the app

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ const cors = require('cors')
 const config = require('../config')
 const PORT = config.port
 const db = require('./utils/database')
+const initModels = require('./models/initModels')
 
 const app = express()
+
+db.authenticate()
+    .then(() => console.log('Database authenticated.'))
+    .catch(err => console.log(err))
+
+db.sync()
+    .then(() => console.log('Database synced.'))
+    .catch(err => console.log(err))
+
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(cors())
---
+++ src/models/users.models.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize')
+const db = require('../utils/database')
+
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [3, 255]
+        }
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [3, 255]
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        index: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    phone: {
+        type: DataTypes.STRING
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATEONLY
+    },
+    role: {
+        type: DataTypes.ENUM('customer', 'admin', 'superadmin'),
+        defaultValue: 'customer'
+    }
+})
+
+module.exports = Users
